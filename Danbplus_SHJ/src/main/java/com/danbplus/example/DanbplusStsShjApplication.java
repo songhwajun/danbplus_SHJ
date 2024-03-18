cfcd8f3972bf6b2c0d63018f9707951a8abeb29a
SongHwaJun <hwajun.song@danbplus.com>
2024-03-18 11:01:35 +0900

    [게시판] SHJ - git 연동 확인

---
+++ Danbplus_SHJ/src/main/java/com/danbplus/example/DanbplusStsShjApplication.java
@@ -0,0 +1,13 @@
+package com.danbplus.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanbplusStsShjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanbplusStsShjApplication.class, args);
+	}
+
+}
